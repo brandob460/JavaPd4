4d0f1acb3a9d908b7cc77bafcc82c81288d44204
brandob460 <brandonb460@nycstudents.net>
2025-09-30 13:45:34 +0000

    weoirytgcuwgf

---
+++ Lesson-03b-Classwork/Main.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+    System.out.println("Brandon Barroso");
+    System.out.println("100-24 207th St");
+    System.out.println("Brandon Barroso \n 100-24 207th St");
+   
+    
+  }
+
+  
+}
